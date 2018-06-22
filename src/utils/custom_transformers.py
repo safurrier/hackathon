f18bc1f4b4f6f6ae5e169379cadd5e1e0648cc14
safurrier <safurrier@gmail.com>
2018-06-22 15:21:02 -0700

    Added FeatureLookupTable transformer that adds features from a lookup table by left joining on specified key columns. Flexible to multiple keys as well as adding prefixes/suffixes to new lookup columns

--- src/utils/custom_transformers.py
+++ src/utils/custom_transformers.py
@@ -113,3 +113,145 @@ class ColumnNameFormatter(TransformerMixin):
     def fit_transform(self, X, y=None):
         """Convenience function performing both fit and transform"""
         return self.fit(X).transform(X)
+
+class FeatureLookupTable(TransformerMixin):
+    """ Given a feature column and path to lookup table, left join the the data
+    and lookup values
+
+    Parameters
+    ----------
+    feature : list
+        A list of the column name(s) of the feature to look up. This is what the lookup table will
+        be left joined on.
+    lookup_key: list
+        A list of the column name(s) the lookup table will be joined on. MUST be in same order
+        as 'feature' arg. If none specified will default to the 'feature' argument.
+    table_lookup_keep_cols: list
+        A list of the columns which should be kept from the joined tables. Default is
+        to keep all columns from table
+    table_path : str
+        The path to the table to use as a lookup
+    table_format : str
+        The type of file the lookup table is. Currently only accepts
+        'csv' (default) or 'pickle'
+    merge_as_string: Boolean
+        Force the key columns to be cast to string before joining
+    add_prefix: str
+        A prefix to add onto the new columns added from the lookup table
+    add_suffix: str
+        A suffix to add onto the new columns added from the lookup table
+
+    """
+    import pandas as pd
+
+    def __init__(self, feature=None, lookup_key=None,
+                 table_lookup_keep_cols=None, table_path=None,
+                 table_format='csv', merge_as_string=False,
+                add_prefix=None, add_suffix=None):
+        self.feature = feature
+        self.table_lookup_keep_cols = table_lookup_keep_cols
+        self.table_path = table_path
+        self.table_format = table_format
+        self.merge_as_string = merge_as_string
+        self.add_prefix = add_prefix
+        self.add_suffix = add_suffix
+
+        # Determine which column to left join the lookup table on
+        if not lookup_key:
+            # If none specified use 'feature'
+            self.lookup_key = self.feature
+        else:
+            self.lookup_key = lookup_key
+
+        if self.table_format == 'csv':
+            self.lookup_table = pd.read_csv(self.table_path)
+        elif self.table_format == 'pickle':
+            self.lookup_table =  pd.read_pickle(self.table_path)
+
+
+    def fit(self, X, y=None):
+        # If transformer has already been fit
+        # and has fitted_data attribute
+        if hasattr(self, 'fitted_data'):
+            # Reload the lookup table because column
+            # names may have been changd
+            if self.table_format == 'csv':
+                self.lookup_table = pd.read_csv(self.table_path)
+            elif self.table_format == 'pickle':
+                self.lookup_table =  pd.read_pickle(self.table_path)
+
+        # Cast dtypes to string if specified
+        if self.merge_as_string:
+            X[self.feature] = X[self.feature].astype(str)
+            self.lookup_table[self.lookup_key] = \
+            self.lookup_table[self.lookup_key].astype(str)
+
+        # Determine which columns to keep from lookup table
+        # If none specified use all the columns in the lookup table
+        if not self.table_lookup_keep_cols:
+            all_cols = self.lookup_table.columns.values.tolist()
+            all_cols = [col for col in all_cols if col not in self.feature]
+            self.table_lookup_keep_cols = all_cols
+
+        # Reduce to only desired columns before merge
+        # Rename lookup_key to the same as self.feature
+        keep_cols = self.table_lookup_keep_cols + self.lookup_key
+        self.lookup_table = self.lookup_table[keep_cols]
+
+        # Renaming dict
+        # Creating a renaming dict to rename the lookup
+        # table keys to the 'feature' keys
+        renaming_dict = dict(zip(self.lookup_key, self.feature))
+        self.lookup_table = self.lookup_table.rename(columns=renaming_dict)
+
+
+        if self.add_prefix:
+            # Don't add oprefix to ORIGINAL table lookup key columns
+            keep_cols = [col for col in keep_cols if col not in self.lookup_key]
+            # Concat the renamed columns (with prefix added) and the key column
+            self.lookup_table = pd.concat([self.lookup_table[keep_cols].add_prefix(self.add_prefix),
+                                           self.lookup_table[self.feature]], axis=1)
+            # Update keep_cols in case adding a suffix also
+            keep_cols = self.lookup_table.columns.values.tolist()
+            # Remove the key column which is now the SAME as SELF.FEATURE
+            keep_cols = [col for col in keep_cols if col not in self.feature]
+
+
+        if self.add_suffix:
+            # Don't add suffix to key columns
+            # Remove the key column which is now the SAME as SELF.FEATURE
+            keep_cols = [col for col in keep_cols if col not in self.feature]
+            # If a prefix has already been added, remove the updated key column
+            # that will have the prefix on it.
+
+            # Concat the renamed columns (with suffix added) and the key column
+            self.lookup_table = pd.concat([self.lookup_table[keep_cols].add_suffix(self.add_suffix),
+                                           self.lookup_table[self.feature]], axis=1)
+
+        # Left join the two
+        new_df = pd.merge(X,
+                self.lookup_table,
+                on=self.feature,
+                          how='left')
+
+        # Assign to attribute
+        self.fitted_data = new_df
+
+        return self
+
+    def transform(self, X, y=None):
+        if hasattr(self, 'fitted_data'):
+            return self.fitted_data
+        else:
+            print('Transformer has not been fit yet')
+
+    def fit_transform(self, X, y=None):
+        if hasattr(self, 'fitted_data'):
+            # Reload the lookup table because column
+            # names may have been changd
+            if self.table_format == 'csv':
+                self.lookup_table = pd.read_csv(self.table_path)
+            elif self.table_format == 'pickle':
+                self.lookup_table =  pd.read_pickle(self.table_path)
+
+        return self.fit(X, y=y).fitted_data
