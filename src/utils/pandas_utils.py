7954b8b76da666060dec63a0d829ba07c63c0aa8
safurrier <safurrier@gmail.com>
2018-07-10 09:54:43 -0700

    added function filter_text_in_column that filters out rows that either include or exclude text from a specified column and a list of strings

--- src/utils/pandas_utils.py
+++ src/utils/pandas_utils.py
@@ -790,4 +790,51 @@ def target_mutual_information(df, target, target_type='categoric', columns=None,
     # Remove instances where a feature was compared with itself
     mmi_df = mmi_df[mmi_df['feature'] != mmi_df['paired_feature']]
     #display(mutual_information_df)
-    return mmi_df     
+    return mmi_df
+
+
+def filter_text_in_column(df, column=None, list_of_strings=None, include_or_exclude='exclude', case=True):
+    """ Given a column and a list of strings, return a dataframe (or original column) where strings are
+        filtered out or filtered to rows where strings are present.
+
+        Caution: Strings are treated as regex inputs.
+
+    Parameters
+    ----------
+    df : Pandas DataFrame
+        A pandas dataframe to search
+    column: str
+        The name of the column to search the strings within
+    list_of_strings: list
+        A list of the strings to use as inputs for regex searching within column
+    include_or_exclude : str --> 'exclude' or 'include'
+        'exclude': Option to return dataframe without rows that match in list of strings
+        'include': Option to return dataframe with rows that match in list of strings
+    case : Boolean
+        Flag for strings to be case sensitive or not
+
+    Returns
+    -------
+    filtered_df: Pandas DataFrame
+        A dataframe filtered down to records where column either matched the list of strings
+        or did not match
+    """
+    # Assertions and argument checking
+    df_copy = df.copy()
+    if not column:
+        return print('Designate column to search within')
+    if not list_of_strings:
+        return print('Designate strings to search for')
+    assert isinstance(column, str)
+    assert isinstance(list_of_strings, list)
+    assert (include_or_exclude == 'exclude') | (include_or_exclude == 'include')
+    # Force column to string type
+    df_copy[column] = df_copy[column].astype(str)
+
+    # Filter Out columns that include/ don't include strings
+    if include_or_exclude == 'exclude':
+        filtered_df = df_copy[~df_copy[column].str.contains('|'.join(list_of_strings), na=False, case=case)]
+    elif include_or_exclude == 'include':
+        filtered_df = df_copy[df_copy[column].str.contains('|'.join(list_of_strings), na=False, case=case)]
+
+    return filtered_df      
