54c02095c32f630bff7f12e91442911fcf7bb610
safurrier <safurrier@gmail.com>
2018-09-04 09:46:45 -0700

    Removed utils from src folder. Install from seperate repo in setup script

--- src/utils/code_snippets/profile_data.py
+++
@@ -1,4 +0,0 @@
-import pandas_profiling
-output_profile_path = r"reports\data-profiling\01-01-initial_data_profiling.html"
-profile = pandas_profiling.ProfileReport(data)
-profile.to_file(outputfile=output_profile_path)
